d563fa2b1e500b60504eed1021e03626dcaf0b3e
Daniel Escobar <escobard90@gmail.com>
2016-12-22 21:37:20 -0700

    type: feat
    added my-app.js main MVC ready for testing

--- src/js/components/my-app.js
+++ src/js/components/my-app.js
@@ -7,7 +7,7 @@
 // for this project, will use the MVC sample defined here : https://alexatnet.com/articles/model-view-controller-mvc-javascript 
 
 // import {main} from '../app.js';
-
+// extend these as new objects from the main MVC in app.js in the future for commonly used functions
 
 	/* ===================================
 
@@ -27,7 +27,7 @@
 
 	======================================*/
 
-		function appView(model, controller){
+		function appView(controller){
 
 			// calls other MVC inits
 			this._controller = appController;
@@ -51,16 +51,26 @@
 			this.view = appView;
 			this.model = appModel;
 
+			// handles the event to open the application menu
 			this._openDrawer = function (){
-				menuButton = _view._elements.menuButton;
+
+				// handles the selectors to open drawer
+				var menuButton = view._elements.menuButton;
+				var drawer = view._elements.drawer;
+
+				// handles the actual event to open the menu 
 				menuButton.addEventListener('tap', function(){
-				   drawerOpened =  new _stateChange(model._drawerState)
+				   drawerOpened = new _stateChange(model._drawerState);
+				   if (drawerOpened === true) {
+				   		drawer.toggle();
+				   }
 				}); 
 			};
 
+			// handles the event to set model states
 			this._stateChange = function (modelElement){
 				 modelElement = true;
 			};
 		};
 
-export default main;
+export default my-app;
