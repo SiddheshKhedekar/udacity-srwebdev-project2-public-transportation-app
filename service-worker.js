e7c2b3404ca03badb188efe5801db093757e9167
Daniel Escobar <escobard90@gmail.com>
2016-12-26 23:07:22 -0700

    type: feat
    added SW updates

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 // the following was imported from own code from notes of the Offline Web Applications course
 
 // sets the cache name
-var staticCacheName = 'pta-static-v5.5';
+var staticCacheName = 'pta-static-v6.2';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
@@ -46,7 +46,25 @@ self.addEventListener('activate', function(event) {
   );
 });
 
+
 self.addEventListener('fetch', function(event) {
+  // TODO: respond to requests for the root page with
+  // the page skeleton from the cache
+  // parses the request URL form the cache
+  var requestURL = new URL(event.request.url);
+
+  // checks to see if the request URL is the same as the current origin
+  if (requestURL.origin === location.origin) {
+    //after the first request is true, checks if the user's request URL is the root
+    if (requestURL.pathname ==='/') {
+      // and if so, responds with the /skeleton URL instead
+      // grabs the /skeleton URL straight, from the cache
+      event.respondWith(caches.match('/'));
+      // no need to go the network as a fallback if this fails, as /skeleton is cached no matter what during the sw install
+      return;
+    }
+  }
+
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
