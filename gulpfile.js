9758f03e313bf50f3cd695c1bf895b6d061073ec
Daniel Escobar <escobard90@gmail.com>
2016-12-28 01:13:22 -0700

    type: feat
    confirmed working latest version of the app

--- gulpfile.js
+++ gulpfile.js
@@ -131,6 +131,6 @@ gulp.task('lint', function () {
 
 // use browser-sync start --server --index index.html --files="public/*.css"
  browserSync.init({
-     server: "./"
+     server: "./public"
  });
  browserSync.stream();
